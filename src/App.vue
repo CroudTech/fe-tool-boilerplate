a411456dd467a3d9366d26f127275cfce7e24f99
Harry <harryndavies@pm.me>
2025-04-28 11:09:09 +0100

    feat: update app title card

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <script setup lang="ts">
 import { RouterView, useRoute } from 'vue-router'
-import { ElContainer, ElMain, ElButton, ElIcon } from 'element-plus'
+import { ElContainer, ElMain, ElButton, ElIcon, ElTag } from 'element-plus'
 import { Plus } from '@element-plus/icons-vue'
 import ToolSectionCard from '@/components/ToolSectionCard.vue'
 import { computed } from 'vue'
 import { RouteName } from '@/router'
+import CuiTag from '@/components/CuiTag.vue'
 
 const route = useRoute()
 
-const navigationButton = computed(() => {
+const navigationButtons = computed(() => {
   switch (route.name) {
     case RouteName.Dashboard:
-      return { text: 'New', route: RouteName.NewRun, primary: true, icon: Plus }
+      return [{ text: 'New', route: RouteName.NewRun, primary: true, icon: Plus }]
     case RouteName.NewRun:
-      return {
-        text: 'Back to dashboard',
-        route: RouteName.Dashboard,
-        primary: false,
-        icon: null,
-      }
+      return [
+        {
+          text: 'Back to dashboard',
+          route: RouteName.Dashboard,
+          primary: false,
+          icon: null,
+        },
+      ]
     case RouteName.Result:
-      return {
-        text: 'Back to dashboard',
-        route: RouteName.Dashboard,
-        primary: false,
-        icon: null,
-      }
+      return [
+        {
+          text: 'Back to dashboard',
+          route: RouteName.Dashboard,
+          primary: false,
+          icon: null,
+        },
+        { text: 'New', route: RouteName.NewRun, primary: true, icon: Plus },
+      ]
     default:
-      return null
+      return []
   }
 })
 </script>
@@ -37,11 +43,18 @@ const navigationButton = computed(() => {
     <ElMain style="display: flex; justify-content: center; height: fit-content">
       <div>
         <ToolSectionCard heading="tool">
-          <h3 class="cui-p-0 cui-mb-0 cui-mb-2 cui-text-black">Tool</h3>
-          <p>This is your new tool description</p>
+          <template #title-append
+            ><ElTag effect="dark" class="cui-tool_tag">Testing</ElTag></template
+          >
+          <h3 class="cui-p-0 cui-mb-2 cui-text-black">Tool</h3>
+          <p>This is your new tool description.</p>
           <template #actions>
-            <RouterLink v-if="navigationButton" :to="{ name: navigationButton.route }">
-              <ElButton :type="navigationButton.primary ? 'primary' : 'default'">
+            <RouterLink
+              v-for="navigationButton in navigationButtons"
+              :key="navigationButton.text"
+              :to="{ name: navigationButton.route }"
+            >
+              <ElButton :type="navigationButton.primary ? 'primary' : 'default'" class="cui-ml-2">
                 {{ navigationButton.text }}
                 <ElIcon v-if="navigationButton.icon" class="cui-ml-2"
                   ><component :is="navigationButton.icon"
@@ -49,9 +62,18 @@ const navigationButton = computed(() => {
               </ElButton>
             </RouterLink>
           </template>
+          <template #actions-prepend>
+            <CuiTag label="Tool tag" />
+          </template>
         </ToolSectionCard>
         <RouterView />
       </div>
     </ElMain>
   </ElContainer>
 </template>
+
+<style lang="scss" scoped>
+:deep(.cui-tool_tag.el-tag) {
+  background-color: #6c72a4;
+}
+</style>
---
+++ src/components/CuiTag.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    /**
+     * Text to be displayed inside tag
+     */
+    label: string
+
+    /**
+     * Font size for the tag
+     * @default '15px'
+     */
+    fontSize?: string
+  }>(),
+  { fontSize: '15px' },
+)
+</script>
+
+<template>
+  <div v-bind="$attrs" class="cui-tag" :style="{ fontSize: fontSize }">
+    {{ label }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cui-tag {
+  @apply cui-inline-block
+         cui-select-none
+         cui-rounded-full;
+
+  white-space: nowrap;
+  padding: 4px 12px;
+  margin: 4px 4px 4px 0px;
+  background-color: #e9e9e9;
+}
+</style>
